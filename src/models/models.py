dd5464d78a15550bbafb6df4486111014bbad4ba
Emmanuel Azuh Mensah <emazuh@nlpg01.cs.washington.edu>
2021-02-08 11:39:22 -0800

    load pretrained models

--- src/models/models.py
+++ src/models/models.py
@@ -141,12 +141,12 @@ class BiEncoderAttentionWithRationaleClassification(nn.Module):
 		self.apply(self._init_weights)
 
 		self.seeker_encoder = SeekerEncoder.from_pretrained(
-								"roberta-base", # Use the 12-layer BERT model, with an uncased vocab.
+								"./weights/pretrained-seeker/", #"roberta-base", # Use the 12-layer BERT model, with an uncased vocab.
 								output_attentions = False, # Whether the model returns attentions weights.
 								output_hidden_states = False)
 
 		self.responder_encoder = ResponderEncoder.from_pretrained(
-								"roberta-base", # Use the 12-layer BERT model, with an uncased vocab.
+								"./weights/pretrained-response/",#"roberta-base", # Use the 12-layer BERT model, with an uncased vocab.
 								output_attentions = False, # Whether the model returns attentions weights.
 								output_hidden_states = False)
 
